bdcc7be9116fa9b73b6f7b9091670b7227701500
Marianna Minich <marry-minich@yandex-team.ru>
2020-07-04 18:05:24 +0300

    add real pokemon's name

--- angular-lab/src/app/pokemon/pokemon-list/mock-data.ts
+++ angular-lab/src/app/pokemon/pokemon-list/mock-data.ts
@@ -1,16 +1,16 @@
 import { Pokemon } from '../../pokemon'; 
 
 export const POKEMONS: Pokemon [] = [
-    {id: 1, name: 'Dr Nice', damage: 50}, 
-    { id: 2, name: 'Narco', damage: 70 },
-    { id: 3, name: 'Bombasto', damage: 46 },
-    { id: 4, name: 'Celeritas', damage: 34 },
-    { id: 5, name: 'Magneta', damage: 78 },
-    { id: 6, name: 'RubberMan', damage: 24 },
-    { id: 7, name: 'Dynama', damage: 12 },
-    { id: 8, name: 'Dr IQ', damage: 9 },
-    { id: 9, name: 'Magma', damage: 43 },
-    { id: 10, name: 'Tornado', damage: 15 },
-    {id: 11, name: 'IQ', damage: 78 },
-    { id: 12, name: 'Ma', damage: 56 },
+    {id: 1, name: 'Bulbasaur', damage: 50}, 
+    { id: 2, name: 'Ivysaur', damage: 70 },
+    { id: 3, name: 'Venusaur', damage: 46 },
+    { id: 4, name: 'Charmander', damage: 34 },
+    { id: 5, name: 'Charmeleon', damage: 78 },
+    { id: 6, name: 'Charizard', damage: 24 },
+    { id: 7, name: 'Squirtle', damage: 12 },
+    { id: 8, name: 'Wartortle', damage: 9 },
+    { id: 9, name: 'Blastoise', damage: 43 },
+    { id: 10, name: 'Caterpie', damage: 15 },
+    {id: 11, name: 'Metapod', damage: 78 },
+    { id: 12, name: 'Butterfree', damage: 56 },
 ];
